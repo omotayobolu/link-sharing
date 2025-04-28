fa31aea76546a5c2ffa180e5ab7fe2445a5ad5cc
omotayobolu <omotayodaniel10@gmail.com>
2025-04-28 12:27:13 +0100

    feat: get request for profile

--- app/api/create-profile/route.ts
+++ app/api/create-profile/route.ts
@@ -1,5 +1,32 @@
 import { createProfile } from "@/lib/db";
+import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId) {
+      return NextResponse.json({ error: "Missing userId" }, { status: 400 });
+    }
+
+    const profile = await prisma.profile.findUnique({
+      where: { userId: userId },
+    });
+
+    if (!profile) {
+      return NextResponse.json(
+        { error: "User's profile not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(profile);
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
 
 export async function POST(request: Request) {
   try {
@@ -22,6 +49,6 @@ export async function POST(request: Request) {
       { status: 201 }
     );
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 400 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
