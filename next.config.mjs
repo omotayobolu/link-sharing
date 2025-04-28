e7c3022f6f4fe35d799d8afa8dbc9898483c56e6
omotayobolu <omotayodaniel10@gmail.com>
2025-04-28 12:24:47 +0100

    feat: added cloudinary domain for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.cache = false;
+    return config;
+  },
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 
